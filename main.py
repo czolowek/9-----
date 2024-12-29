a06c8a8208f92e5cab9f2181cb2f9ffa713ff6df
czolowek <klutoi39@gmail.com>
2024-12-29 12:23:52 +0100

    u8ygoi

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from uuid import uuid4
-import sched
 
 from flask import Flask, render_template, redirect, request, url_for, flash
 
@@ -11,14 +10,14 @@ app = Flask(__name__)
 app.secret_key = "password"
 
 
-@app.route("/add_tur/", methods=["GET", "POST"])
+@app.route("/add_room/", methods=["GET", "POST"])
 def add_room():
     if request.method == "POST":
         with Session() as session:
             number = request.form.get("number")
             floor = request.form.get("floor")
             type = request.form.get("type")
-            squire = request.form.get("squire")
+            square = request.form.get("square")
             img_name_origin = None
             img_name = None
             img_url = None
@@ -34,7 +33,7 @@ def add_room():
                 number=number,
                 floor=floor,
                 type=type,
-                squire=squire,
+                square=square,
                 img_name_origin=img_name_origin,
                 img_name=img_name,
                 img_url=img_url
@@ -54,14 +53,14 @@ def index():
         return render_template("index.html", rooms=rooms)
 
 
-@app.get("/tur/<int:room_id>")
+@app.get("/room/<int:room_id>")
 def get_room(room_id: int):
     with Session() as session:
         room = session.query(Room).where(Room.id == room_id).first()
         return render_template("room.html", room=room)
 
 
-@app.route("/edit_tur/<int:room_id>", methods=["GET", "POST"])
+@app.route("/edit_room/<int:room_id>", methods=["GET", "POST"])
 def edit_room(room_id: int):
     with Session() as session:
         room = session.query(Room).where(Room.id == room_id).first()
@@ -70,27 +69,30 @@ def edit_room(room_id: int):
             room.number = request.form.get("number")
             room.floor = request.form.get("floor")
             room.type = request.form.get("type")
-            room.squire = request.form.get("squire")
+            room.square = request.form.get("square")
+            room.reserved = True if request.form.get("reserved") else False
 
             file = request.files.get("img")
             if file and file.filename:
                 room.img_name_origin = file.filename
                 room.img_name = uuid4().hex + "." + file.filename.split(".")[-1]
-                img_url = f"/static/img/{room.img_name}"
-                file.save(img_url)
+                room.img_url = f"static/img/{room.img_name}"
+                file.save(room.img_url)
 
             session.commit()
+            flash("Інформацію про кімнату успішно оновлено")
             return redirect(url_for("get_room", room_id=room_id))
 
         return render_template("edit_room.html", room=room)
 
 
-@app.get("/del_tur/<int:room_id>")
+@app.get("/del_room/<int:room_id>")
 def del_room(room_id: int):
     with Session() as session:
         room = session.query(Room).where(Room.id == room_id).first()
         session.delete(room)
         session.commit()
+        flash(f"Кімнату з номером {room.number} успішно видалено")
         return redirect(url_for("index"))
 
 
@@ -106,4 +108,4 @@ def reserve(room_id: int):
 
 if __name__ == "__main__":
     create_db()
-    app.run(debug=True, port=5051)
+    app.run(debug=True, port=8080)
